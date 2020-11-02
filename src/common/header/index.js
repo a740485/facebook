00fe581ac78fc274631ecfd0fe43ac7aa5fca817
傅文皓 <a740485@gmail.com>
2020-11-02 16:40:57 +0800

    header畫面

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,38 +1,93 @@
 import React from "react";
 import { connect } from "react-redux";
-import { HeaderWrapper } from "./style";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faSearch,
+    faHome,
+    faStore,
+    faUsers,
+    faGamepad,
+    faPlus,
+    faBell,
+    faCaretDown,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+    faFacebook,
+    faYoutube,
+    faFacebookMessenger,
+} from "@fortawesome/free-brands-svg-icons";
 import { actionCreators } from "./store";
+import { Link } from "react-router-dom";
+import {
+    HeaderWrapper,
+    Left,
+    Search,
+    Center,
+    Right,
+    CenterItem,
+    UserInfo,
+    UserImg,
+    UserName,
+    RightItem,
+} from "./style";
 
 class Header extends React.Component {
     render() {
-        const { loginState, handleTestClick, apiData } = this.props;
-        console.log(loginState);
         return (
             <HeaderWrapper>
-                header: {loginState}
-                <button onClick={handleTestClick}>test</button>
-                <p>{apiData}</p>
+                <Left>
+                    <Link to="/">
+                        <FontAwesomeIcon className="logo" icon={faFacebook} />
+                    </Link>
+
+                    <FontAwesomeIcon className="search" icon={faSearch} />
+                    <Search />
+                </Left>
+                <Center>
+                    <CenterItem>
+                        <FontAwesomeIcon className="item" icon={faHome} />
+                    </CenterItem>
+                    <CenterItem>
+                        <FontAwesomeIcon className="item" icon={faYoutube} />
+                    </CenterItem>
+                    <CenterItem>
+                        <FontAwesomeIcon className="item" icon={faStore} />
+                    </CenterItem>
+                    <CenterItem>
+                        <FontAwesomeIcon className="item" icon={faUsers} />
+                    </CenterItem>
+                    <CenterItem>
+                        <FontAwesomeIcon className="item" icon={faGamepad} />
+                    </CenterItem>
+                </Center>
+                <Right>
+                    <UserInfo>
+                        <UserImg />
+                        <UserName>一二三</UserName>
+                    </UserInfo>
+                    <RightItem>
+                        <FontAwesomeIcon className="item" icon={faPlus} />
+                    </RightItem>
+                    <RightItem>
+                        <FontAwesomeIcon
+                            className="item"
+                            icon={faFacebookMessenger}
+                        />
+                    </RightItem>
+                    <RightItem>
+                        <FontAwesomeIcon className="item" icon={faBell} />
+                    </RightItem>
+                    <RightItem>
+                        <FontAwesomeIcon className="item" icon={faCaretDown} />
+                    </RightItem>
+                </Right>
             </HeaderWrapper>
         );
     }
-
-    componentDidMount() {
-        this.props.getData();
-    }
 }
 
-const mapState = (state) => ({
-    loginState: state.getIn(["header", "loginState"]),
-    apiData: state.getIn(["header", "apiData"]),
-});
+const mapState = (state) => ({});
 
-const mapDispatch = (dispatch) => ({
-    handleTestClick() {
-        dispatch(actionCreators.testAction());
-    },
-    getData() {
-        console.log("dispatch: getData()");
-        dispatch(actionCreators.getDataInfo());
-    },
-});
+const mapDispatch = (dispatch) => ({});
 export default connect(mapState, mapDispatch)(Header);
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -1,7 +1,142 @@
 import styled from "styled-components";
 
 export const HeaderWrapper = styled.div`
-    width: 300px;
+    width: 100%;
     height: 56px;
-    background-color: #ddd;
+    /* background-color: #ddd; */
+    position: fixed;
+    box-shadow: 0 0 8px #ddd;
+`;
+
+export const Left = styled.div`
+    width: 360px;
+    height: 56px;
+    float: left;
+    box-sizing: border-box;
+    /* padding: 0 20px; */
+    background-color: none;
+    display: flex;
+    align-items: center;
+    position: relative;
+
+    .logo {
+        width: 40px;
+        height: 40px;
+        margin-left: 13px;
+    }
+
+    .search {
+        position: absolute;
+        left: 72px;
+        top: 20px;
+        color: rgb(91, 93, 97);
+        z-index: 1;
+    }
+`;
+
+export const Search = styled.input.attrs({
+    placeholder: "搜尋 Facebook",
+})`
+    width: 280px;
+    height: 40px;
+    box-sizing: border-box;
+    position: relative;
+    background-color: rgb(238, 240, 243);
+    padding-left: 35px;
+    border-radius: 40px;
+    line-height: 30px;
+    font-size: 17px;
+    border: none;
+    margin-left: 7px;
+    &::placeholder {
+        color: #999;
+    }
+    &:focus {
+        border: none;
+        outline: none;
+    }
+`;
+
+export const Center = styled.div`
+    width: 600px;
+    height: 100%;
+    text-align: center;
+    margin: 0 auto;
+    position: absolute;
+    left: 0;
+    right: 0;
+    z-index: 0;
+    display: flex;
+    align-items: center;
+`;
+
+export const CenterItem = styled.div`
+    width: calc(600px / 5);
+    height: 100%;
+    line-height: 30px;
+    font-size: 25px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    &:hover {
+        height: 50px;
+        background: rgb(242, 242, 242);
+        border-radius: 5px;
+    }
+    .item {
+        color: rgb(172, 173, 175);
+    }
+`;
+
+export const Right = styled.div`
+    width: 320px;
+    height: 100%;
+    float: right;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+`;
+
+export const UserInfo = styled.div`
+    height: 36px;
+    display: flex;
+    align-items: center;
+    padding: 5px;
+    box-sizing: border-box;
+    border-radius: 60px;
+    cursor: pointer;
+    :hover {
+        background: rgb(242, 242, 242);
+    }
+`;
+
+export const UserImg = styled.div`
+    height: 28px;
+    width: 28px;
+
+    background: rgb(17, 241, 144);
+    border-radius: 100%;
+`;
+
+export const UserName = styled.div`
+    height: 19px;
+    display: inline-block;
+    margin: 0 10px;
+    color: #050505;
+`;
+
+export const RightItem = styled.div`
+    height: 40px;
+    width: 40px;
+    border-radius: 50%;
+    background: rgb(222, 224, 230);
+    margin-left: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    :hover {
+        background: rgb(213, 214, 220);
+    }
 `;
